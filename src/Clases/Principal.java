ac1c951f7119a2586dcfc886882abc232320c292
William Alexander Matallana <wmatallana28@gmail.com>
2023-02-10 20:10:06 -0500

    Se crearon variables doble

--- src/Clases/Principal.java
+++ src/Clases/Principal.java
@@ -6,10 +6,15 @@ public class Principal {
 
         System.out.println("Hola Clase");
 
-        int a = 5, b=6;
+        int a = 5, b = 6;
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
+
+        double c = 3.14, d = 5.68;
+
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
 
 
     }
